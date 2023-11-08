c395f1a69e9d8d7aa0d2398c2ddcb2f5438da2df
losiik <49565548+losiik@users.noreply.github.com>
2023-11-09 03:27:30 +0400

    refactor routs

--- backend/app.py
+++ backend/app.py
@@ -47,9 +47,9 @@ def registration():
         db.session.add(user)
         db.session.commit()
 
-        access_token = create_access_token(identity=email)
+        #access_token = create_access_token(identity=email)
 
-        return {'success': True, 'access_token': access_token}, 200
+        return {'success': True}, 200#, 'access_token': access_token}, 200
     except IntegrityError:
         db.session.rollback()
         return {'success': False}, 400
