ce864bef1407016e43921446486ad227c881026b
losiik <49565548+losiik@users.noreply.github.com>
2023-11-09 02:08:09 +0400

    refactor routs

--- backend/app.py
+++ backend/app.py
@@ -20,64 +20,55 @@ db.init_app(app)
 api = Api(app)
 
 
+@app.route('/registration', methods=['POST'])
 @cross_origin()
-class UserRegistration(Resource):
-    def post(self):
-        data = request.get_json()
+def registration():
+    data = request.get_json()
 
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        password = data.get('password')
-        email = data.get('email')
-        phone_number = data.get('phone_number')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    password = data.get('password')
+    email = data.get('email')
+    phone_number = data.get('phone_number')
 
-        role = Role.query.filter_by(role='абитуриент').first()
+    role = Role.query.filter_by(role='абитуриент').first()
 
-        try:
-            user = User(
-                email=email,
-                password=password,
-                first_name=first_name,
-                last_name=last_name,
-                phone_number=phone_number,
-                role_id=role.id
-            )
-            db.session.add(user)
-            db.session.commit()
+    try:
+        user = User(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            phone_number=phone_number,
+            role_id=role.id
+        )
+        db.session.add(user)
+        db.session.commit()
 
-            access_token = create_access_token(identity=email)
+        access_token = create_access_token(identity=email)
 
-            return {'success': True, 'access_token': access_token}, 200
-        except IntegrityError:
-            db.session.rollback()
-            return {'success': False}, 400
+        return {'success': True, 'access_token': access_token}, 200
+    except IntegrityError:
+        db.session.rollback()
+        return {'success': False}, 400
 
 
+@app.route('/login', methods=['POST'])
 @cross_origin()
-class UserLogin(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('email', type=str, required=True, help='Email is required')
-        parser.add_argument('password', type=str, required=True, help='Password is required')
-        args = parser.parse_args()
+def login():
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', type=str, required=True, help='Email is required')
+    parser.add_argument('password', type=str, required=True, help='Password is required')
+    args = parser.parse_args()
 
-        password = args['password']
-        email = args['email']
+    password = args['password']
+    email = args['email']
 
-        user = User.query.filter_by(email=email, password=password).first()
-        if user:
-            access_token = create_access_token(identity=email)
-            return {'access_token': access_token, 'role_id': user.role_id}
-        return {'message': 'Invalid credentials'}, 401
-
-
-api.add_resource(UserRegistration, '/registration')
-api.add_resource(UserLogin, '/login')
-
-
-@app.route('/')
-def hello_world():
-    return 'Moe Flask приложение в контейнере Docker.'
+    user = User.query.filter_by(email=email, password=password).first()
+    if user:
+        access_token = create_access_token(identity=email)
+        return {'access_token': access_token, 'role_id': user.role_id}
+    return {'message': 'Invalid credentials'}, 401
 
 
 if __name__ == '__main__':
