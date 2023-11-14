cc5b14eee28cf21ac381811674ef3fe046c45942
losiik <49565548+losiik@users.noreply.github.com>
2023-11-14 11:08:53 +0300

    rm test files

--- backend/test/test.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-# Генерировать новый UUID
-new_uuid = uuid.uuid4()
-
-# Преобразовать UUID в строку
-uuid_str = str(new_uuid)
-
-print("Сгенерированный UUID:", uuid_str)
--- backend/test/test_request.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-r = requests.get('http://localhost:9000/get_all_courses')
-print(r.json())
